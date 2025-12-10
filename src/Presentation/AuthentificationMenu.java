19b878178b8448e76d7fbc37f7e955cc6a590fe1
Solange <solangepaulina.bel@students.salle.url.edu>
2025-12-10 15:08:20 +0100

    Made authentication menu, which will delegate the user input stuff to the controller if I'm not mistaken, we'll double check with Edu.

--- src/Presentation/AuthentificationMenu.java
+++ src/Presentation/AuthentificationMenu.java
@@ -1,4 +1,19 @@
 package Presentation;
 
 public class AuthentificationMenu {
+    private ClientController clientController;
+
+    public AuthentificationMenu(ClientController clientController) {
+        this.clientController = clientController;
+    }
+
+    public void printAuthenticationMenu() {
+        System.out.println("- Authentication Menu -");
+        System.out.println("    1) Login");
+        System.out.println("    2) Sign up");
+        System.out.println("    0) Exit");
+        System.out.print("\nChoose an option: ");
+
+        clientController.handleUserInput();
+    }
 }
