61fa28394fbb6cc3fbe9132114500233574ea87e
solangepbr <solangepaulina.bel@students.salle.url.edu>
2026-06-24 11:03:12 +0200

    added logging command to silence api in terminal

--- src/Presentation/main.java
+++ src/Presentation/main.java
@@ -11,6 +11,8 @@ public class main {
      * @param args command line arguments
      */
     public static void main(String[] args) {
+        java.util.logging.LogManager.getLogManager().reset(); // Logging command to silence API response in terminal.
+
         MenuController controller = new MenuController();
         if(!controller.start()) System.out.println("\nERROR: Some files don't exist or contain errors in their structure.");
     }
